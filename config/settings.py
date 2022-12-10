59c0e967c8d0e0d0f87a168e145755175f9e962d
Aleksandr Timofeev <tauman08@gmail.com>
2022-12-10 13:13:50 +0300

    lesson_8 i18n

--- config/settings.py
+++ config/settings.py
@@ -230,3 +230,4 @@ CELERY_RESULT_BACKEND = "redis://localhost:6379"
 # Email as files for debug
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = "var/email-messages/"
+LOCALE_PATHS = [BASE_DIR / "locale"]
